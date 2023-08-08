1d78874a029f26e0023d175a82bf8ff78b717185
Noemi Radu <noemiradu12@gmail.com>
2023-08-08 16:28:43 +1000

    2nd ch. done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # Read all
+  # get "/tasks", to: "tasks#index", as: :tasks
+
+  # # Add a new task
+  # post "tasks", to: "tasks#create"
+  # get "/tasks/new", to: "tasks#new", as: :tasks_new
+
+  # # Read one
+  # get "/tasks/:id", to: "tasks#show", as: :task
+
+  # # Edit one
+  # patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :tasks_edit
+
+  # # Delete one
+  # delete "tasks/:id", to: "tasks#destroy", as: :tasks_destroy
+
+  resources :tasks
 end
--- db/migrate/20230808025448_create_tasks.rb
+++ db/migrate/20230808025448_create_tasks.rb
@@ -9,3 +9,5 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     end
   end
 end
+
+# migrated
